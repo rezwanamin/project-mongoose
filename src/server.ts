d2422812667a08e89202bbf16b69b1a3d8be9183
sarhady <rezwan20a@gmail.com>
2023-05-13 22:31:21 +0600

    Updated

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 5000;
+
+//using cors
+app.use(cors());
+
+//database connection
+async function bootstrap() {
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
+        console.log('Database Connection Successful');
+
+        app.get('/', (req, res) => {
+            res.send('Hello World!')
+          });
+    }catch(err){
+        console.log(`failed to connect database`, err);
+    };
+
+  };
+
+bootstrap();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
